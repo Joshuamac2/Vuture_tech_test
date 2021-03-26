11738eb4e5326bdf64a45da119aceb148ce474f0
Joshua <joshuamacleod@live.com>
2021-03-26 13:37:32 +0000

    task two complete

--- spec/taskTwoSpec.js
+++ spec/taskTwoSpec.js
@@ -1,7 +1,13 @@
-describe("palindrom", () => {
+describe("palindromic", () => {
   it("should return true if the string is a Palindrome", () => {
     let palindromicString = "God saved Eva’s dog";
+    let notPalindromicString = "I have some cheese";
 
-    let isPalindrome = palindromic(palindromicString);
+    let isPalindromic = palindromic(palindromicString);
+    let isNotPalindromic = palindromic(notPalindromicString);
+
+
+    expect(isPalindromic).toEqual(true);
+    expect(isNotPalindromic).toEqual(false);
   });
 });
--- src/taskTwo.js
+++ src/taskTwo.js
@@ -1,3 +1,6 @@
-function palindromic() {
-  
-}
+function palindromic(string) {
+  var rexStr = /[\W_]/g;
+  var lowStr = string.toLowerCase().replace(rexStr, '');
+  var reverseStr = lowStr.split('').reverse().join('');
+  return reverseStr === lowStr;
+};
