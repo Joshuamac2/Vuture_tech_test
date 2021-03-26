59e364b29256aa369ae2ef17929d898e9531960c
Joshua <joshuamacleod@live.com>
2021-03-26 12:37:27 +0000

    First red green refactor for task one

---
+++ spec/taskOneSpec.js
@@ -0,0 +1,5 @@
+describe("countElement", () => {
+  it("should return number of occurrences of a given letter in a string", () => {
+    let ouccurancesInString = countElement("I have some cheese", "e");
+  })
+})
