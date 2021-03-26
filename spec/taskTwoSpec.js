0072433d9052f0afc3dfc325ec4bddeb5ce30427
Joshua <joshuamacleod@live.com>
2021-03-26 13:04:04 +0000

    first red green refactor for task two

---
+++ spec/taskTwoSpec.js
@@ -0,0 +1,7 @@
+describe("palindrom", () => {
+  it("should return true if the string is a Palindrome", () => {
+    let palindromicString = "God saved Eva’s dog";
+
+    let isPalindrome = palindromic(palindromicString);
+  });
+});
