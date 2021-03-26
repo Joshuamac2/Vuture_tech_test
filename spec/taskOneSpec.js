7f4cccb1f1c90121531ab6293451af389f9c186d
Joshua <joshuamacleod@live.com>
2021-03-26 12:46:02 +0000

    task one passes

--- spec/taskOneSpec.js
+++ spec/taskOneSpec.js
@@ -1,5 +1,9 @@
 describe("countElement", () => {
   it("should return number of occurrences of a given letter in a string", () => {
     let ouccurancesInString = countElement("I have some cheese", "e");
+    let ouccurancesInStringTwo = countElement("I have some cheese", "s");
+
+    expect(ouccurancesInString).toEqual(5);
+    expect(ouccurancesInStringTwo).toEqual(2);
   })
-})
+});
--- src/taskOne.js
+++ src/taskOne.js
@@ -1,3 +1,10 @@
-function countElement() {
+function countElement(string, letter) {
+  let count = 0;
 
+  for(let i = 0; i < string.length; i++) {
+    if (string.charAt(i) == letter) {
+      count += 1;
+    }
+  }
+  return count;
 };
