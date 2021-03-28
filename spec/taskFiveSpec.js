7cfd12f1f1965f9d1521785efac504be4556e3fc
Joshua <joshuamacleod@live.com>
2021-03-29 00:44:31 +0100

    Task 5, first red green refactor

---
+++ spec/taskFiveSpec.js
@@ -0,0 +1,5 @@
+describe("censorPalindrome", () => {
+  it("should censor words that are palindromic from a string", () => {
+    let censorPalindromicWords = censorPalindrome("Anna went to vote in the election to fulfil her civic duty");
+  })
+});
