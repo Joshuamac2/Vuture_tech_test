7213f8eab6ba2e9451790319be85e189b6753d4e
Joshua <joshuamacleod@live.com>
2021-03-28 12:45:19 +0100

    task 3 first red green

---
+++ spec/taskThreeSpec.js
@@ -0,0 +1,5 @@
+describe("listedOccurrence", () => {
+  it("should return number of occurences of a given list of words and its total", () => {
+    let oouccurancesTotal = listedOccurrence(["dog", "cat", "large"], "I have a cat named Meow and a dog name Woof. I love the dog a lot. He is larger than a small horse.");
+  })
+});
