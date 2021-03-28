f97dd5bc1a0f2f2e45ae702a29043a7bd86369df
Joshua <joshuamacleod@live.com>
2021-03-28 21:12:55 +0100

    Task 4 complete

--- spec/taskFourSpec.js
+++ spec/taskFourSpec.js
@@ -2,5 +2,7 @@ describe("censorList", () => {
   it("should censor words from a given list", () => {
     let censorTheseWords = censorList(["Meow", "Woof"], "I have a cat named Meow and a dog name Woof. I love the dog a lot. He is larger than a small horse.");
 
+    expect(censorTheseWords).toEqual("I have a cat named M##w and a dog name W##f. I love the dog a lot. He is larger than a small horse.");
+    expect(censorTheseWords).not.toEqual("I have a cat named Meow and a dog name Woof. I love the dog a lot. He is larger than a small horse.");
   })
 });
--- src/taskFour.js
+++ src/taskFour.js
@@ -1,3 +1,5 @@
 function censorList(list, string) {
+  const reg = RegExp('\\b'+list.join`\\b|\\b`+'\\b','gi')
 
-}
+  return string.replace(reg, m=> m[0] + '#'.repeat(m.length / 2) + m.charAt(m.length - 1));
+};
