14d6063744959ef19d8b966fcbef981a182d0795
Joshua <joshuamacleod@live.com>
2021-03-28 19:38:49 +0100

    Task 4 first red green refactor

---
+++ spec/taskFourSpec.js
@@ -0,0 +1,6 @@
+describe("censorList", () => {
+  it("should censor words from a given list", () => {
+    let censorTheseWords = censorList(["Meow", "Woof"], "I have a cat named Meow and a dog name Woof. I love the dog a lot. He is larger than a small horse.");
+
+  })
+});
