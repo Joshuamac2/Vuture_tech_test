1e9b64463e755ed8a75ace6314e2a721efd98111
Joshua <joshuamacleod@live.com>
2021-03-28 13:14:08 +0100

    task 3 complete

--- spec/taskThreeSpec.js
+++ spec/taskThreeSpec.js
@@ -1,5 +1,9 @@
 describe("listedOccurrence", () => {
   it("should return number of occurences of a given list of words and its total", () => {
     let oouccurancesTotal = listedOccurrence(["dog", "cat", "large"], "I have a cat named Meow and a dog name Woof. I love the dog a lot. He is larger than a small horse.");
+    let oouccurancesTotalFalse = listedOccurrence(["Woof", "Meow"], "I have a cat named Meow and a dog name Woof. I love the dog a lot. He is larger than a small horse.");
+
+    expect(oouccurancesTotal).toEqual({cat: 1, dog: 2, large: 1, total: 4});
+    expect(oouccurancesTotalFalse).not.toEqual({cat: 1, dog: 2, large: 1, total: 4});
   })
 });
--- src/taskThree.js
+++ src/taskThree.js
@@ -1,4 +1,11 @@
 
-function listedOccurrence() {
+function listedOccurrence(list, string) {
+  let regex = new RegExp(list.join("|"), "g"),
+      matches = string.match(regex),
+      pair = {total: matches.length},
+      counter = {};
 
-};
+  matches.forEach(m => counter[m] = counter[m] + 1 || 1);
+
+  return {...counter, ...pair};
+}
