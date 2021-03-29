b2258dbf525dfa081e1c036c9cfbd7b9448b3f81
Joshua <joshuamacleod@live.com>
2021-03-29 01:12:16 +0100

    Task 5 complete

--- spec/taskFiveSpec.js
+++ spec/taskFiveSpec.js
@@ -1,5 +1,9 @@
 describe("censorPalindrome", () => {
   it("should censor words that are palindromic from a string", () => {
     let censorPalindromicWords = censorPalindrome("Anna went to vote in the election to fulfil her civic duty");
+
+    expect(censorPalindromicWords).toEqual("A$$a went to vote in the election to fulfil her c$$$c duty");
+    expect(censorPalindromicWords).not.toEqual("Anna went to vote in the election to fulfil her civic duty");
+
   })
 });
--- src/taskFive.js
+++ src/taskFive.js
@@ -1,3 +1,17 @@
-function censorPalindrome(){
-  
+function censorPalindrome(string) {
+  let arrayValues = string.toLowerCase().split(' ');
+
+  let palindromes = arrayValues.filter(w => {
+    let len = w.length;
+    for (let i = 0; i < len / 2; i++) {
+      if (w[i] == w[len - i - 1]) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  });
+
+  const reg = RegExp('\\b'+palindromes.join`\\b|\\b`+'\\b','gi')
+  return string.replace(reg, m=> m[0] + '$'.repeat(m.length / 1.5) + m.charAt(m.length - 1));
 }
