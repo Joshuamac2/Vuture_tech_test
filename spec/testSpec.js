72a2fbaaa006f9be952caa3ede0950e3cb577306
Joshua <joshuamacleod@live.com>
2021-04-16 11:35:38 +0100

    Unit test passes without user input to change frequency settings

--- spec/testSpec.js
+++ spec/testSpec.js
@@ -1,12 +1,8 @@
-// input = [60,10,45,60,1500]
-// output = [60,40,45,60,1000]
-
 describe('frequency', () => {
   it("should bring frequency to max number of 1000", () => {
     let input = [60,10,45,60,1500];
-
     let frequencyWithInput = frequency(input);
-    //
-    // expect(frequencyWithInput).toEqual([60,40,45,60,1000]);
+
+    expect(frequencyWithInput).toEqual([60,40,45,60,1000]);
   })
 })
--- src/test.js
+++ src/test.js
@@ -1,3 +1,10 @@
 function frequency(input) {
-
+  for (var i = 0; i < input.length; i++){
+    if(input[i] < 40 ){
+      input[i] = 40;
+    }else if(input[i] > 1000){
+      input[i] = 1000;
+    }
+  }
+  return input;
 };
